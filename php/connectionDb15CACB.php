2378e2c6c876dc87229f070bf585b651684ebd66
adityasanil <aditya.sanil@somaiya.edu>
2019-07-03 12:51:51 +0530

    Sign up page

---
+++ php/connectionDb15CACB.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username   = "sanil";
+$password   = "";
+$dbname     = "15CACB";
+
+$connect = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$connect) {
+    die("Connection failed to database failed! Error: " . mysqli_connect_error());
+}
+else {
+    echo "Database connected";
+}
+?>
