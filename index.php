9c5221d9cc03a780f5967b8843dae8115dc0231d
Jigar Thakkar <jigar.kt@somaiya.edu>
2019-07-13 22:25:21 +0530

    Footer Added

--- index.php
+++ index.php
@@ -12,8 +12,14 @@ session_start();
         <meta name="viewport" content="width=device-width, initial-scale=1"> 
         <link rel="icon" type="png" href="images/15.png">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    
+        <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0-11/css/all.min.css">
         <link href='styles/navbarStyles.css' rel="stylesheet">
+        <link href="styles/footerStyle.css" rel="stylesheet">
+
     </head>
 
     <body>
@@ -22,7 +28,7 @@ session_start();
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <a class="navbar-brand" href="">
+                <a class="navbar-brand">
                     <img src="images/15cacb.png" width="" height="30" class="d-inline-block align-top" alt="">
                 </a>
 
@@ -70,10 +76,17 @@ session_start();
                 </div>
             </div>
         </div>
-    
-    
-    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>        
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <footer>
+                <div class="container">
+		        <p class="cpr">&copy 2019 15CACB Pvt. Ltd.
+				<a href=""><img class="smedia" src="images/fb.jpg" height="30" width="30" hspace="5"/></a>
+  				<a href=""><img class="smedia" src="images/tw.jpg" height="30" width="30" hspace="5"/></a>
+  				<a href=""><img class="smedia" src="images/ln.jpg" height="30" width="30" hspace="5"/></a>
+  			</p>
+  			<span class="contact">contact@15cacb.com</span>
+  			<span class="contact">+91 9967110003</span>
+  		</div>
+  	</footer>
     </body>
 </html>
+								
