bd601839046782ad4f12077b1ea855a61c631c72
Jigar Thakkar <jigar.kt@somaiya.edu>
2019-07-26 21:11:37 +0530

    CASELAWS

--- routes/caselaws.php
+++ routes/caselaws.php
@@ -1,81 +1,152 @@
 <?php
 // session_start()
-// $$sessionHolder = $_SESSION['user'];
 
-// echo "Session holder: " . $sessionHolder;
+
+include 'php/connectionDb15CACB.php';
+$result = mysqli_query($connect, "SELECT * FROM posts where selectedOption='caselaw'");
+
 
 ?>
 <html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Caselaws</title>
+        <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script> 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>      
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
-        <link href='../../styles/navbarStyles.css' rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet">
+        <style>
+            .col-md-8{
+                font-family: 'Oswald', sans-serif;
+                font-size: 20px;
+                font-weight: 200;
+           }
+            .card-img-top {
+                width: 100%;
+                height: 20vh;
+                object-fit: cover;
+            }
+        </style>
     </head>
 
     <body>
-        
-        <div class="container">
-            <h3 class="title lead mt-2" style="">Caselaws</h3>
-        </div>
+        <br>
+        <h3 class="">Caselaws</h3>
         <div class="card-deck">
-
-            <div class="col-sm-6 col-md-5 col-lg-3">
-                <div class="card shadow mb-3" style="width: 16rem;">
-                    <img class="card-img-top" src="images/space.jpeg" alt="Card image">
+                <?php
+                    while ($row = mysqli_fetch_array($result)) 
+                    {
+                    ?>
+                    <div class="col-sm-6 col-md-5 col-lg-3">
+                    <div class="card shadow mb-3" style="width: 16rem;">
+                    <?php echo '<img class="card-img-top" src="data:image;base64,'.base64_encode($row["image"]).'" alt="Card image">' ; ?>
                     <div class="card-body">
-                        <h5 class="card-title stretched-link">Card 1</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. </p>
-                        <a href="https://www.google.com" class="stretched-link" style="display: hidden" target="_blank"></a>
-                        <p class="card-text"><small class="text-muted text-success">Date of posting</small></p>
+                        <h5 class="card-title"><?php echo $row["title"]; ?></h5>
+                        <p class="card-text"><small class="text-muted text-success">Date of posting:&nbsp<?php echo $row["date"];?></small></p>
+                        <input type="button" name="view" value="view" id="<?php echo $row["post_id"]; ?>" class="btn btn-info btn-xs view_data" />
+
                     </div>
-                </div>
-             </div>
-
-             <div class="col-sm-6 col-md-5 col-lg-3">
-                <div class="card shadow mb-3" style="width: 16rem;">
-                    <img class="card-img-top" src="images/space.jpeg" alt="Card image">
-                    <div class="card-body">
-                        <h5 class="card-title stretched-link">Card 1</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. </p>
-                        <a href="https://www.google.com" class="stretched-link" style="display: hidden" target="_blank"></a>
-                        <p class="card-text"><small class="text-muted text-success">Date of posting</small></p>
                     </div>
-                </div>
-             </div>
-
-             <div class="col-sm-6 col-md-5 col-lg-3">
-                <div class="card shadow mb-3" style="width: 16rem;">
-                    <img class="card-img-top" src="images/space.jpeg" alt="Card image">
-                    <div class="card-body">
-                        <h5 class="card-title stretched-link">Card 1</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. </p>
-                        <a href="https://www.google.com" class="stretched-link" style="display: hidden" target="_blank"></a>
-                        <p class="card-text"><small class="text-muted text-success">Date of posting</small></p>
                     </div>
-                </div>
-             </div>
-
-             <div class="col-sm-6 col-md-5 col-lg-3">
-                <div class="card shadow mb-3" style="width: 16rem;">
-                    <img class="card-img-top" src="images/space.jpeg" alt="Card image">
-                    <div class="card-body">
-                        <h5 class="card-title stretched-link">Card 1</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. </p>
-                        <a href="https://www.google.com" class="stretched-link" style="display: hidden" target="_blank"></a>
-                        <p class="card-text"><small class="text-muted text-success">Date of posting</small></p>
-                    </div>
-                </div>
-             </div>
-
-            
-        </div>
-
-
-
-
+                    <?php
+                    }
+                    ?>
+       </div>
     </body>
 </html>
+
+
+
+<div id="dataModal" class="modal fade">  
+      <div class="modal-dialog modal-xl modal-dialog-scrollable">  
+           <div class="modal-content">  
+                <div class="modal-header">    
+                     <h4 class="modal-title">Caselaw</h4>  
+                </div>
+                <div class="modal-body" id="article_detail">  
+                </div>  
+                <div class="modal-footer">  
+                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>  
+                </div>  
+           </div>  
+      </div>  
+ </div>  
+
+
+<script>  
+ $(document).ready(function(){  
+      $('.view_data').click(function(){  
+           var article_id = $(this).attr("id");  
+           $.ajax({  
+                url:"php/select.php",  
+                method:"post",  
+                data:{article_id:article_id},  
+                success:function(data){  
+                     $('#article_detail').html(data);  
+                     $('#dataModal').modal("show");  
+                }  
+           });  
+      });  
+ });  
+ </script>
+
+
+    <!--         <div class="col-sm-6 col-md-4 col-lg-3">
+                <div class="card mb-4" style="width: 16rem;">
+                    <img class="card-img-top" src="images/space.jpeg" alt="Card image">
+                    <div class="card-body">
+                        <h5 class="card-title stretched-link">Card 1</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. </p>
+                        <a href="https://www.google.com" class="stretched-link" style="display: hidden" target="_blank"></a>
+                        <p class="card-text"><small class="text-muted text-success">Date of posting</small></p>
+                    </div>
+                </div>
+             </div>
+
+             <div class="col-sm-6 col-md-4 col-lg-3">
+                <div class="card mb-4" style="width: 16rem;">
+                    <img class="card-img-top" src="images/space.jpeg" alt="Card image">
+                    <div class="card-body">
+                        <h5 class="card-title stretched-link">Card 2</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. </p>
+                        <a href="https://www.google.com" class="stretched-link" style="display: hidden" target="_blank"></a>
+                        <p class="card-text"><small class="text-muted text-success">Date of posting</small></p>
+                    </div>
+                </div>
+             </div>
+
+             <div class="col-sm-6 col-md-4 col-lg-3">
+                <div class="card mb-4" style="width: 16rem;">
+                    <img class="card-img-top" src="images/space.jpeg" alt="Card image">
+                    <div class="card-body">
+                        <h5 class="card-title stretched-link">Card 3</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. </p>
+                        <a href="https://www.google.com" class="stretched-link" style="display: hidden" target="_blank"></a>
+                        <p class="card-text"><small class="text-muted text-success">Date of posting</small></p>
+                    </div>
+                </div>
+             </div>
+
+             <div class="col-sm-6 col-md-4 col-lg-3">
+                <div class="card mb-4" style="width: 16rem;">
+                    <img class="card-img-top" src="images/space.jpeg" alt="Card image">
+                    <div class="card-body">
+                        <h5 class="card-title stretched-link">Card 4</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. </p>
+                        <a href="https://www.google.com" class="stretched-link" style="display: hidden" target="_blank"></a>
+                        <p class="card-text"><small class="text-muted text-success">Date of posting</small></p>
+                    </div>
+                </div>
+             </div>
+             
+        
+        </div>-->
+        
+
+
+        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
--- routes/createBlog.php
+++ routes/createBlog.php
@@ -1,5 +1,45 @@
 <?php 
-// session_start();
+session_start();
+
+
+$sessionHolder=$_SESSION['user'];
+
+include 'php/connectionDb15CACB.php';
+
+if(isset($_POST["insert"]))  
+ {
+
+    $sql = "SELECT * FROM Users WHERE username = '$sessionHolder'";
+    $querySql = mysqli_query($connect, $sql);
+    $row = mysqli_fetch_array($querySql);
+
+    $firstName = $row['firstName'];
+    $lastName = $row['lastName'];
+    $identity = $row['identity'];
+    $file = addslashes(file_get_contents($_FILES["image"]["tmp_name"]));
+    $option=$_POST['selectbox'];
+    $title = $_POST['title'];
+    $content = $_POST['image_text'];
+    $date = date("D\, jS M Y");    
+    $query = "INSERT INTO posts(firstName, lastName, identity, selectedOption, title, content, image, date) VALUES ('$firstName','$lastName','$identity','$option','$title','$content','$file', '$date')";
+    if(mysqli_query($connect, $query))  
+      {  
+       echo '<script>alert("Post created successfully")</script>';  
+      }  
+
+
+
+      /*$file = addslashes(file_get_contents($_FILES["image"]["tmp_name"])); 
+      $title = $_POST['title'];
+      $content = $_POST['image_text'];
+      $date = date("D\, jS M Y");
+      $query = "INSERT INTO caselaw(firstName, title, content, image, date) VALUES ('$sessionHolder', $title','$content','$file', '$date')";  
+      if(mysqli_query($connect, $query))  
+      {  
+           echo '<script>alert("Image Inserted into Database")</script>';  
+      }  */
+ } 
+
 
 ?>
 <html>
@@ -8,13 +48,67 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
         <link href='styles/navbarStyles.css' rel='stylesheet'>
+        <link href="https://cdn.jsdelivr.net/npm/froala-editor@3.0.0-beta.1/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" />
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/froala-editor@3.0.0-beta.1/js/froala_editor.pkgd.min.js"></script>
     </head>
 
     <body>
         <div class="container">
-            <p class="lead">Create blog</p>
+            <h1>Create blog</h1><br>
+            <form method="post" enctype="multipart/form-data">
+                <div class="form-group"><br>
+                    <label><h5>Select what you want to create(Caselaw or Rule):</h3></label>  
+                    <select name="selectbox">
+                        <option value="Rule" name="rule">Rule</option>
+                        <option value="Caselaw" name="caselaw">Caselaw</option>
+                    </select><br><br>
+                    <div class="input-group">
+                        <textarea class="form-control" id="title" placeholder="Enter the title here (MAX 100 CHARACTERS)" type="text" rows="1" name="title" required></textarea> 
+                    </div><br>
+                    <textarea 
+                     id="text" 
+                     rows="4" 
+                     name="image_text" ></textarea>
+                        <script>
+                        var editor = new FroalaEditor('#text', {
+                        toolbarButtons: {
+                        'moreText': {
+                        'buttons': ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', 'textColor', 'backgroundColor', 'inlineClass', 'inlineStyle', 'clearFormatting']
+                            },
+                            'moreParagraph': {
+                                'buttons': ['quote']
+                            },
+                            'moreRich': {
+                                    'buttons': ['insertLink', 'insertTable', 'specialCharacters', 'embedly', 'insertHR']
+                            },
+                            'moreMisc': {
+                            'buttons': ['undo', 'redo', 'fullscreen', 'print', 'getPDF', 'spellChecker', 'selectAll', 'html', 'help'],'align': 'right','buttonsVisible': 2
+                            }
+                        },
+                        quickInsertTags: []
+
+                        });
+                        </script>
+ 
+                    </div>
+                    <div class="form-group">
+                        <div class="form-row float-right">
+                            <div class="col-auto">
+                            <span class='label label-info mt-2 mr-2' id="uploadFileInfo"></span>
+                            <label class="btn btn-success" for="myFileSelector">
+                                <input id="myFileSelector" type="file" style="display:none" name="image" onchange="$('#uploadFileInfo').html(this.files[0].name)">
+                                Upload Image
+                            </label>
+                            </div>
+                            <div class="col-auto">
+                            <input type="submit" class="btn btn-success" id="insert" name="insert" value="Submit">
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     </body>
 </html>
