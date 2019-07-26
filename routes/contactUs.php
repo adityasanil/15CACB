dd6be548c853efdcac83e68a18fccfb3c9586b51
Jigar Thakkar <jigar.kt@somaiya.edu>
2019-07-26 21:20:42 +0530

    contactUs complete

--- routes/contactUs.php
+++ routes/contactUs.php
@@ -10,6 +10,11 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
         <link href='styles/navbarStyles.css' rel='stylesheet'>
+        <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
     </head>
 
     <body>
@@ -33,7 +38,7 @@
                     <br>
                     <div class="card" style="border: none;">
                     <article class="card-body">
-                        <form class="form">
+                        <form class="gform" id="Contactus" action="https://script.google.com/macros/s/AKfycbwqiueJt4tly5z1-Vl_cpaB4MtlyxM7x--7boN7vBjp2eVs_kAW/exec" method="POST">
                             <div class="form-group">
                                 <div class="input-group">
                                     <input class="form-control" placeholder="Your name" type="text" name="name" required>
@@ -56,11 +61,17 @@
                             </div>
                             <div class="form-group float-right">
                                 <button type="submit" class="btn btn-success" name="querySubmit">Submit</button>
+                            </div>                            
+                            <div class="thankyou_message" style="display:none;">
+                                <h4><em>Thanks</em> for contacting us!</h4>
+                                <h4>Our team will get back to you soon!</h4>
                             </div>
+                            
                         </form>
                     </article>    
                 </aside>
             </div>
-        </div> 
+        </div>
+        <script data-cfasync="false" type="text/javascript" src="https://cdn.rawgit.com/dwyl/html-form-send-email-via-google-script-without-server/master/form-submission-handler.js"></script>
     </body>
 </html>
